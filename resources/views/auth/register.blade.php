29f7e6d95c5f5dd419c9851ba7e7df00cd0174d8
Nenad998 <69111430+Nenad998@users.noreply.github.com>
2020-10-26 22:15:07 +0100

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,11 +5,11 @@
     <div class="contact_main_login">
         <div class="contact_section_login">
 
-            @error('ime')  @enderror{{ old('ime') }} @error('ime')
+             @error('name')
             <div class="register_name">{{ $message }}</div>
             @enderror
 
-            @error('password')  @enderror{{ old('password') }} @error('password')
+            @error('password')
             <span class="invalid-feedback" role="alert">
                                         {{ $message }}
                                     </span>
@@ -24,13 +24,13 @@
                         @csrf
 
                         <div class="input-fields_login">
-                            <input id="name" type="text"  class="input" placeholder="Ime">
+                            <input id="name" type="text" name="name" class="input" placeholder="Ime">
 
-                            <input id="email" type="email"   class="input" placeholder="Email Adresa">
+                            <input id="email" type="email" name="email"  class="input" placeholder="Email Adresa">
 
 
 
-                            <input id="password" type="password"  @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" class="input" placeholder="Lozinka">
+                            <input id="password" type="password"  @error('password') is-invalid @enderror name="password" required autocomplete="new-password" class="input" placeholder="Lozinka">
                             @error('password')
 
                             @enderror
